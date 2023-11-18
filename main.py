d602ad4745fab1ba0da22b7f74651c627e048bf3
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-11-19 04:19:11 +0530

    remove cookies from payload

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from scraper.scrape import Scrape
 import pprint
 pprint = pprint.PrettyPrinter(indent=4).pprint
 scraper = Scrape()
-res = scraper.scrape_unstop()
+res = scraper.scrape_devfolio()
+pprint(res)
 
